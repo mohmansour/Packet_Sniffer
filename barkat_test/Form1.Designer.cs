4ad4bc7c3b8c99aa0d48a29e6727239bdf3959e9
mohamed mansour <mohamed.mnsoor_1995@yahoo.com>
2017-12-24 00:25:49 +0200

    commit

---
+++ barkat_test/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Packet_Sniffer_test
+{
+    partial class GUI
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Select_but = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Source_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Destination_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Protocol_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Length_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Info_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Select_but
+            // 
+            this.Select_but.Location = new System.Drawing.Point(12, 12);
+            this.Select_but.Name = "Select_but";
+            this.Select_but.Size = new System.Drawing.Size(75, 23);
+            this.Select_but.TabIndex = 0;
+            this.Select_but.Text = "Select";
+            this.Select_but.UseVisualStyleBackColor = true;
+            this.Select_but.Click += new System.EventHandler(this.Select_but_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 103);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(13, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "capture";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "stop";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.Time_,
+            this.Source_,
+            this.Destination_,
+            this.Protocol_,
+            this.Length_,
+            this.Info_});
+            this.dataGridView1.Location = new System.Drawing.Point(192, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 219);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            // 
+            // Time_
+            // 
+            this.Time_.HeaderText = "Time";
+            this.Time_.Name = "Time_";
+            // 
+            // Source_
+            // 
+            this.Source_.HeaderText = "Source";
+            this.Source_.Name = "Source_";
+            // 
+            // Destination_
+            // 
+            this.Destination_.HeaderText = "Destination";
+            this.Destination_.Name = "Destination_";
+            // 
+            // Protocol_
+            // 
+            this.Protocol_.HeaderText = "Protocol";
+            this.Protocol_.Name = "Protocol_";
+            // 
+            // Length_
+            // 
+            this.Length_.HeaderText = "Length";
+            this.Length_.Name = "Length_";
+            // 
+            // Info_
+            // 
+            this.Info_.HeaderText = "Info";
+            this.Info_.Name = "Info_";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 313);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Select_but);
+            this.Name = "GUI";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Select_but;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Source_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Destination_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Protocol_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Length_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Info_;
+    }
+}
+
---
+++ barkat_test/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpPcap;
+using PacketDotNet;
+using System.Windows.Forms;
+using SharpPcap.WinPcap;
+using System.Net;
+
+namespace Packet_Sniffer_test
+{
+    public partial class GUI : Form
+     {
+        public string uy;           // Array of devices
+        CaptureDeviceList devices;  //Global selected device
+        ICaptureDevice selected;
+        int packet_number = 0;      //counter to count packet
+        String source;              //source ip
+        string Number;              //counter number but in string(convert packet_number to string)
+        string Time;                //string to time
+        String destination;         //destination ip
+        string protocol;            //string for protocol type
+        String length;              //string for packet length
+        String type;                //type of packet(ethernet...)
+        public string info;         //string contains all packet information
+        public GUI()
+         {
+             InitializeComponent();
+             show_all_Adapters();
+             Select_but.Click += Button1_Click;
+             button2.Click += on_start_scan;
+             button3.Click += on_stop_event;
+         }
+
+         private void on_stop_event(object sender, EventArgs e)
+         {
+             selected.StopCapture();
+             selected.Close();
+         }
+
+         private void on_start_scan(object sender, EventArgs e)
+         {
+            try
+            {
+                selected.OnPacketArrival += selected_OnPacketArrival;
+                selected.Open(DeviceMode.Promiscuous, 100);
+                selected.StartCapture();
+            }
+            catch (Exception ex)
+            {MessageBox.Show("Make sure you already selected a valid device\n\n" + ex.ToString());}
+        }
+        private void selected_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+
+            Packet p = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            type = p.GetType().ToString();
+            String y = p.ToString();
+            Console.WriteLine(y);
+            if (p != null)
+            {
+             int counter = 0;
+             string temp = " ";
+             int flag1 = 0;
+             int flag2 = 0;
+             int flag3 = 0;
+             for (int i = 0; i < y.Length - 1; i++)
+                 {
+                 if (y[i] == '=' | counter == 4 | counter == 5 | counter == 7)
+                    {
+                     if (counter == 4 && flag1 == 0)
+                        {
+                         if (y[i + 1] != ',') { temp = temp + y[i]; }
+                         else
+                            {
+                             temp = temp + y[i];
+                             source = temp;
+                             temp = " ";
+                             flag1 = 1;
+                            }
+                        }
+                     else if (counter == 5 && flag2 == 0)
+                             {
+                              if (y[i + 1] != ',') {temp = temp + y[i];}
+                              else
+                              {
+                               temp = temp + y[i];
+                               destination = temp;
+                               temp = " ";
+                               flag2 = 1;
+                              }
+                            }
+                     else if (counter == 7 && flag3 == 0)
+                            {
+                             if (y[i + 1] != ',') {temp = temp + y[i];}
+                             else
+                             {
+                              temp = temp + y[i];
+                              protocol = temp;
+                              temp = " ";
+                              flag3 = 1;
+                             }
+                            }
+                        else if (y[i] == '=') {counter++;}
+                    }
+                }
+
+
+                int len = e.Packet.Data.Length;
+                length = len.ToString();
+                info = p.ToString();
+                DateTime time = e.Packet.Timeval.Date;
+                Number = packet_number.ToString();
+                Time = time.Hour.ToString() + " : " + time.Minute.ToString() + " : " + time.Second.ToString() + " : " + time.Millisecond.ToString();
+
+                packet_number++;
+
+                if (dataGridView1.InvokeRequired)
+                {
+                    dataGridView1.Invoke((MethodInvoker)delegate ()
+                    {
+                        dataGridView1.Rows.Add(Number,Time,source,destination,protocol,length,info);
+                    });
+                }
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+         {
+            try
+             {
+                 selected = devices[listView1.SelectedIndices[0]];
+                 button2.Enabled = true;
+            }
+            catch (Exception ex)
+             {MessageBox.Show("Make sure you already selected a valid device\n\n" + ex.ToString());}
+        }
+
+         void show_all_Adapters()
+         {
+             devices = CaptureDeviceList.Instance;
+             foreach (var dev in devices)
+             {
+              listView1.Items.Add(dev.Description);
+             }
+         }
+
+        private void Select_but_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+        
+        }
+    } 
+ }
+    
